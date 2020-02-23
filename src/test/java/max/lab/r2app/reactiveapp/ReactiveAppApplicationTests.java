07515a61c30de73685bfd05d9998e8c92d3f031f
Max Yuan <max.m.yuan@gmail.com>
2020-02-23 18:42:16 +0800

    add the tests

---
+++ src/test/java/max/lab/r2app/reactiveapp/ReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package max.lab.r2app.reactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
